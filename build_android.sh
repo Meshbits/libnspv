f3290ac66499adaa07f32fa03a52e24149a55324
ns408 <nsrea408@gmail.com>
2020-01-21 01:27:32 +1100

    create functions to run selectively

--- build_android.sh
+++ build_android.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # compile openssl, libevent with-openssl, libsodium and libnspv for
 # aarch64-linux-android on a macOS Mojave 10.14.6
+# Ensure https://developer.android.com/studio/projects/install-ndk
+# todo:
+# - do we need to split this
+# - ndk can be installed from cli
+
 set -xe
 
 SCRIPTPATH=`realpath .`
-
-OPENSSL_VERSION=openssl-1.1.1d
-OPENSSL_DIR=$SCRIPTPATH/$OPENSSL_VERSION
-OPENSSL_INSTALL_DIR=$SCRIPTPATH/${OPENSSL_VERSION}_install
 
 export ANDROID_API=28
 export ARCHITECTURE=android-arm64
@@ -24,63 +25,91 @@ export LD=$TOOLCHAIN/bin/aarch64-linux-android-ld
 export RANLIB=$TOOLCHAIN/bin/aarch64-linux-android-ranlib
 export STRIP=$TOOLCHAIN/bin/aarch64-linux-android-strip
 
-PATH=$TOOLCHAIN/bin:$PATH
+export PATH=$TOOLCHAIN/bin:$PATH
+
+OPENSSL_VERSION=openssl-1.1.1d
+OPENSSL_DIR=$SCRIPTPATH/$OPENSSL_VERSION
+OPENSSL_INSTALL_DIR=$SCRIPTPATH/${OPENSSL_VERSION}_install
 
 # openssl
-rm -rf $OPENSSL_INSTALL_DIR $OPENSSL_VERSION
-wget -c https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz
-wget https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz.sha256
-echo "$(cat ${OPENSSL_VERSION}.tar.gz.sha256) ${OPENSSL_VERSION}.tar.gz" | sha256sum --check --status
-tar xvzf ${OPENSSL_VERSION}.tar.gz
-cd $OPENSSL_DIR
-./Configure $ARCHITECTURE -D__ANDROID_API__=$ANDROID_API \
-  --prefix=$OPENSSL_INSTALL_DIR
-make build_libs
-# Don't install manpages
-make install_sw
+function openssl_install() {
+  rm -rf $OPENSSL_INSTALL_DIR $OPENSSL_VERSION
+  wget -c https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz
+  wget -c https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz.sha256
+  echo "$(cat ${OPENSSL_VERSION}.tar.gz.sha256) ${OPENSSL_VERSION}.tar.gz" | sha256sum --check --status
+  tar xvzf ${OPENSSL_VERSION}.tar.gz
+  cd $OPENSSL_DIR
+  ./Configure $ARCHITECTURE -D__ANDROID_API__=$ANDROID_API \
+    --prefix=$OPENSSL_INSTALL_DIR
+  make build_libs
+  # Don't install manpages
+  make install_sw
+}
 
-# libevent
 LIBEVENT_INSTALL_DIR=$SCRIPTPATH/libevent_install
 
-rm -rf libevent
-git clone https://github.com/libevent/libevent.git
-cd libevent
-make clean || true
+# libevent
+function libevent_install() {
+  cd $SCRIPTPATH
+  rm -rf libevent
+  git clone https://github.com/libevent/libevent.git
+  cd libevent
+  make clean || true
 
-export PKG_CONFIG_PATH=$OPENSSL_INSTALL_DIR/lib/pkgconfig
-./autogen.sh
+  export PKG_CONFIG_PATH=$OPENSSL_INSTALL_DIR/lib/pkgconfig
+  ./autogen.sh
 
-CPPFLAGS="-I$OPENSSL_INSTALL_DIR/include" \
-  LDFLAGS="-L$OPENSSL_INSTALL_DIR/lib" \
-  ./configure --host $target_host \
-    --prefix=$LIBEVENT_INSTALL_DIR
-
-make \
   CPPFLAGS="-I$OPENSSL_INSTALL_DIR/include" \
-  LDFLAGS="-L$OPENSSL_INSTALL_DIR/lib"
-make install
+    LDFLAGS="-L$OPENSSL_INSTALL_DIR/lib" \
+    ./configure --host $target_host \
+      --prefix=$LIBEVENT_INSTALL_DIR
 
-# Compile and build libsodium-1.0.18-stable.tar.gz
+  make \
+    CPPFLAGS="-I$OPENSSL_INSTALL_DIR/include" \
+    LDFLAGS="-L$OPENSSL_INSTALL_DIR/lib"
+  make install
+}
+
 LIBSODIUM_INSTALL_DIR=$SCRIPTPATH/libsodium_install
-rm -rf libsodium-stable $LIBSODIUM_INSTALL_DIR
-wget -c https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable.tar.gz
-tar xvzf libsodium-1.0.18-stable.tar.gz
-cd libsodium-stable
-export LDFLAGS='--specs=nosys.specs'
-export CFLAGS='-Os'
-./configure --host=arm-none-eabi --prefix=$LIBSODIUM_INSTALL_DIR
-make
-make install
+
+# Compile and build libsodium
+function libsodium_install() {
+  cd $SCRIPTPATH
+  rm -rf libsodium-stable $LIBSODIUM_INSTALL_DIR
+  wget -c https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable.tar.gz
+  tar xvzf libsodium-1.0.18-stable.tar.gz
+  cd libsodium-stable
+  export LDFLAGS='--specs=nosys.specs'
+  export CFLAGS='-Os'
+  ./configure --host=arm-none-eabi --prefix=$LIBSODIUM_INSTALL_DIR
+  make
+  make install
+}
 
 # Compile and build libnspv
+function libnspv_install() {
+  cd $SCRIPTPATH
+  rm -rf libnspv
+  git clone https://github.com/dimxy/libnspv.git
+  cd libnspv
+  git checkout myCC
+  ./autogen.sh
+  ./configure --enable-androidso \
+    --host=$target_host \
+    CFLAGS="-I$LIBEVENT_INSTALL_DIR/include -I$LIBSODIUM_INSTALL_DIR/include" \
+    LDFLAGS="-L$LIBEVENT_INSTALL_DIR/lib -L$LIBSODIUM_INSTALL_DIR/lib"
+  cd src/tools/cryptoconditions
+  ./autogen.sh
+  ./configure --enable-androidso=yes \
+    --host=$target_host \
+    CFLAGS="-I$LIBEVENT_INSTALL_DIR/include -I$LIBSODIUM_INSTALL_DIR/include" \
+    LDFLAGS="-L$LIBEVENT_INSTALL_DIR/lib -L$LIBSODIUM_INSTALL_DIR/lib"
+  make
+  cd ../../..
+  make
+}
 
-rm -rf libnspv
-git clone https://github.com/dimxy/libnspv.git
-cd libnspv
-git checkout myCC
-./autogen.sh
-./configure --enable-androidso \
-  --host=$target_host \
-  CFLAGS="-I$LIBEVENT_INSTALL_DIR/include -I$LIBSODIUM_INSTALL_DIR/include" \
-  LDFLAGS="-L$LIBEVENT_INSTALL_DIR/lib -L$LIBSODIUM_INSTALL_DIR/lib"
-make check
+openssl_install
+libevent_install
+libsodium_install
+libnspv_install
